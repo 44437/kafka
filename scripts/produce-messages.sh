25f2d4419f4a3b076208e07bb72f4778cdff4ad5
44437 <ercantopuz33@gmail.com>
2024-10-06 20:01:36 +0300

    Activate multiple brokers, and display all of them on the conduktor monitor

--- scripts/produce-messages.sh
+++ scripts/produce-messages.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-docker exec -it kafka-broker-1 sh -c "opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server host.docker.internal:9092 --topic first --property parse.key=true --property key.separator=="
+docker exec -it kafka-broker-1-1 sh -c "opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server host.docker.internal:9092 --topic first --property parse.key=true --property key.separator=="
