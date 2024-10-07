1489e13186baa1b88ea7ddb2a815fd1ad950dc60
44437 <ercantopuz33@gmail.com>
2024-10-07 21:50:09 +0300

    cleanup.policy

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -2,3 +2,4 @@
 
 docker exec -it kafka-broker-1-1 sh -c "opt/bitnami/kafka/bin/kafka-topics.sh --create --topic first --bootstrap-server host.docker.internal:9092 --partitions 3 --replication-factor 1"
 # for compression in the topic level: `--config compression.type=lz4`
+# for cleanup.policy: compact, delete can be used together `--config cleanup.policy=compact,delete --config retention.ms=604800000`
