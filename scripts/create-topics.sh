b8d83139e2f4d17731ad0ab1841eac8cb62e1bbb
44437 <ercantopuz33@gmail.com>
2024-10-07 21:14:03 +0300

    Compress the messages in the topic level

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,3 +1,4 @@
 # !/bin/bash
 
 docker exec -it kafka-broker-1-1 sh -c "opt/bitnami/kafka/bin/kafka-topics.sh --create --topic first --bootstrap-server host.docker.internal:9092 --partitions 3 --replication-factor 1"
+# for compression in the topic level: `--config compression.type=lz4`
