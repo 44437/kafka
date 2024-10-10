a14c4c3be0729639b2888e8cf1cc18825b45d8a1
44437 <ercantopuz33@gmail.com>
2024-10-10 20:40:28 +0300

    retention.policy

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -3,3 +3,4 @@
 docker exec -it kafka-broker-1-1 sh -c "opt/bitnami/kafka/bin/kafka-topics.sh --create --topic first --bootstrap-server host.docker.internal:9092 --partitions 3 --replication-factor 1"
 # for compression in the topic level: `--config compression.type=lz4`
 # for cleanup.policy: compact, delete can be used together `--config cleanup.policy=compact,delete --config retention.ms=604800000`
+# for retention.policy: size limit, time can be used together `--config retention.bytes=104857600 --config retention.ms=604800000`
