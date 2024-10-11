ea06a5b06bc87a690cebf643d89f50c108f7cfe7
44437 <ercantopuz33@gmail.com>
2024-10-11 18:52:00 +0300

    Adjust the script create-topics

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-docker exec -it kafka-broker-1-1 sh -c "opt/bitnami/kafka/bin/kafka-topics.sh --create --topic first --bootstrap-server host.docker.internal:9092 --partitions 3 --replication-factor 1"
+docker exec -it kafka-broker-0-1 sh -c "opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic-0 --bootstrap-server host.docker.internal:9092"
 # for compression in the topic level: `--config compression.type=lz4`
 # for cleanup.policy: compact, delete can be used together `--config cleanup.policy=compact,delete --config retention.ms=604800000`
 # for retention.policy: size limit, time can be used together `--config retention.bytes=104857600 --config retention.ms=604800000`
